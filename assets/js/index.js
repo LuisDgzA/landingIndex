f5bffeb1b82a83acfe70ded9b181ef1538f8f1da
LuisDgzA <luisdgza96@gmail.com>
2022-12-02 12:40:59 -0600

    agrego js

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const navlinkEmpresasParticipantes = document.getElementById(
+    "navlinkEmpresasParticipantes"
+);
+
+navlinkEmpresasParticipantes.addEventListener("mouseover", () => {
+    document.querySelector(".menu-empresas-participantes").style.display =
+        "block";
+});
