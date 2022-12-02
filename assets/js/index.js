588888fe8c8b607eeedc2e6fa2dce628df1cf640
LuisDgzA <luisdgza96@gmail.com>
2022-12-02 12:52:12 -0600

    index.js mouseout

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,8 @@ navlinkEmpresasParticipantes.addEventListener("mouseover", () => {
     document.querySelector(".menu-empresas-participantes").style.display =
         "block";
 });
+
+navlinkEmpresasParticipantes.addEventListener("mouseout", () => {
+    document.querySelector(".menu-empresas-participantes").style.display =
+        "none";
+});
