dc09b0704f8ffc1a5016facdfbfb49f517bec28f
LuisDgzA <luisdgza96@gmail.com>
2022-12-06 13:54:12 -0600

    carousel listo

--- index.php
+++ index.php
@@ -15,6 +15,53 @@
 
     <main>
 		<section id="carousel">
+			<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+				<div class="carousel-indicators">
+					<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+					<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+				</div>
+				<div class="carousel-inner">
+					<div class="carousel-item active">
+						<img src="./assets/img/carousel1.jpg" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-lg-block carousel-custom-container">
+							<h3 class="title-carousel">1r item</h3>
+							<div class="section-info-carousel mt-4">
+								Información 1 aquí
+							</div>
+							<button class="btn-carousel-formar-parte mt-4">Quiero formar parte</button>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img src="./assets/img/carousel2.jpg" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-lg-block carousel-custom-container">
+							<h3 class="title-carousel">1r item</h3>
+							<div class="section-info-carousel mt-4">
+								Información 2 aquí
+							</div>
+							<button class="btn-carousel-formar-parte mt-4">Quiero formar parte</button>
+						</div>
+					</div>
+					<div class="carousel-item">
+						<img src="./assets/img/carousel3.jpg" class="d-block w-100" alt="...">
+						<div class="carousel-caption d-none d-lg-block carousel-custom-container">
+							<h3 class="title-carousel">1r item</h3>
+							<div class="section-info-carousel mt-4">
+								Información 3 aquí
+							</div>
+							<button class="btn-carousel-formar-parte mt-4">Quiero formar parte</button>
+						</div>
+					</div>
+				</div>
+				<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Previous</span>
+				</button>
+				<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="visually-hidden">Next</span>
+				</button>
+			</div>
 			<div class="menu-empresas-participantes border-red">
 
 			</div>
