96d6785229edfeab438833a65e23d7c27793b04e
LuisDgzA <luisdgza96@gmail.com>
2022-12-06 11:18:05 -0600

    funcionamiento menu de empresas participantes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,27 @@
-const navlinkEmpresasParticipantes = document.getElementById(
-    "navlinkEmpresasParticipantes"
-);
+const navlinkEmpresasParticipantes = document.getElementById("navlinkEmpresasParticipantes");
+
+const menuEmpresasParticipantes = document.querySelector(".menu-empresas-participantes");
+
+const quitarMenu = document.querySelectorAll(".quitar-menu");
 
 navlinkEmpresasParticipantes.addEventListener("mouseover", () => {
-    document.querySelector(".menu-empresas-participantes").style.display =
-        "block";
+    document.querySelector(".menu-empresas-participantes").style.display = "block";
+    // document.querySelector(".topbar-menu").addEventListener("mouseout", function(e){
+    //     e.stopPropagation();
+    //     if (document.querySelector(".menu-empresas-participantes").style.display == "block") {
+    //         document.querySelector(".menu-empresas-participantes").style.display = "none";
+    //     }
+    // })
 });
 
-navlinkEmpresasParticipantes.addEventListener("mouseout", () => {
-    document.querySelector(".menu-empresas-participantes").style.display =
-        "none";
+menuEmpresasParticipantes.addEventListener("mouseout", () => {
+    document.querySelector(".menu-empresas-participantes").style.display = "none";
+});
+
+quitarMenu.forEach((quitarMenuItem) => {
+    quitarMenuItem.addEventListener("mouseover", function () {
+        if (document.querySelector(".menu-empresas-participantes").style.display == "block") {
+            document.querySelector(".menu-empresas-participantes").style.display = "none";
+        }
+    });
 });
--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="topbar color-white shadow">
-        <div class="topbar-top w-100 d-flex">
+        <div class="topbar-top w-100 d-flex quitar-menu">
             <div class="topbar-top__contacto d-flex align-items-center">
                 <div class="topbar-top__container d-flex justify-content-between">
                 <div class="topbar-top__items">
@@ -18,36 +18,36 @@
                 ingreso alumnos
                 </button>
             </div>
-            </div>
-            <div class="topbar-menu d-flex align-items-center">
-            <div class="topbar-menu__home all-centered">
+        </div>
+        <div class="topbar-menu d-flex align-items-center">
+            <div class="topbar-menu__home all-centered quitar-menu">
                 <img src="./assets/iconos/index-negro.svg" alt="icono-index" />
             </div>
             <div class="topbar-menu__navbar">
                 <nav>
-                <ul class="d-flex justify-content-around">
-                    <li>
-                        <a href="">Inicio</a>
-                    </li>
-                    <li>
-                        <a href="" id="navlinkEmpresasParticipantes">Empresas participantes</a>
-                    </li>
-                    <li>
-                        <a href="preparatoria.php">Preparatoria</a>
-                    </li>
-                    <li>
-                        <a href="licenciatura.php">Licenciatura</a>
-                    </li>
-                    <li>
-                        <a href="">Noticias</a>
-                    </li>
-                    <li>
-                        <a href="">Directorio</a>
-                    </li>
-                    <li>
-                        <a href="aliados.php">Aliados</a>
-                    </li>
-                </ul>
+                    <ul class="d-flex justify-content-around">
+                        <li>
+                            <a href="" class="quitar-menu">Inicio</a>
+                        </li>
+                        <li>
+                            <a href="" class="" id="navlinkEmpresasParticipantes">Empresas participantes</a>
+                        </li>
+                        <li>
+                            <a href="preparatoria.php" class="quitar-menu">Preparatoria</a>
+                        </li>
+                        <li>
+                            <a href="licenciatura.php" class="quitar-menu">Licenciatura</a>
+                        </li>
+                        <li>
+                            <a href="" class="quitar-menu">Noticias</a>
+                        </li>
+                        <li>
+                            <a href="" class="quitar-menu">Directorio</a>
+                        </li>
+                        <li>
+                            <a href="aliados.php" class="quitar-menu">Aliados</a>
+                        </li>
+                    </ul>
                 </nav>
             </div>
         </div>
